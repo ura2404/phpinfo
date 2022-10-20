e285caeaf59823d2bf2b48a8767a0e1792098ab2
root <root@tilda-db10.urx.su>
2022-10-20 17:13:24 +0300

    .

--- var/www/phpinfo/index.php
+++ var/www/phpinfo/index.php
@@ -3,6 +3,8 @@
 		<div style="display:flex">
 			<div style="margin:auto">
 				<a href="opcache">Opcache</a>
+				<span style="color:lightgrey">|</span>
+				<a href="bench.php">Bench</a>
 			</div>
 		</div>
 			<hr/>

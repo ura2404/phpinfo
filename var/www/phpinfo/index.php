8f3d4a7495068ad924160f4ff677f7092fe97a8c
root <root@db8-src-qcow2.urx.su>
2022-10-20 17:29:25 +0300

    .

--- var/www/phpinfo/index.php
+++ var/www/phpinfo/index.php
@@ -1,14 +1,17 @@
 <?php
 	print '	<html>
-		<div style="display:flex">
-			<div style="margin:auto">
-				<a href="opcache">Opcache</a>
-				<span style="color:lightgrey">|</span>
-				<a href="bench.php">Bench</a>
-			</div>
+	<head>
+		<title>phpinfo</title>
+	</head>
+	<div style="display:flex">
+		<div style="margin:auto">
+			<a href="opcache">Opcache</a>
+			<span style="color:lightgrey">|</span>
+			<a href="bench.php">Bench</a>
 		</div>
-			<hr/>
-	</html>	';
+	</div>
+	<hr/>
+</html>	';
 
 	phpinfo();
 ?>

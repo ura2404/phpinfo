8c1f0949a8c8d4da3625c2fe59c0704ead40d226
ura <ura@local-web1.urx.su>
2025-10-22 15:40:54 +0300

    оптимизация для php fpm

--- var/www/phpinfo/index.php
+++ var/www/phpinfo/index.php
@@ -1,19 +1,19 @@
 <?php
-	$version = phpversion();
+$version = phpversion();
 
-	print '	<html>
-	<head>
-		<title>phpinfo &bull; '.$version.'</title>
-	</head>
-	<div style="display:flex">
-		<div style="margin:auto">
-			<a href="/opcache">Opcache</a>
-			<span style="color:lightgrey">|</span>
-			<a href="bench.php">Bench</a>
-		</div>
-	</div>
-	<hr/>
+print '	<html>
+<head>
+    <title>phpinfo &bull; '.$version.'</title>
+</head>
+    <div style="display:flex;margin-top:20px;">
+        <div style="margin:auto">
+            <a href="'. str_replace('phpinfo', 'opcache', $_SERVER['REQUEST_URI']) .'">Opcache</a>
+            <span style="color:lightgrey">|</span>
+            <a href="bench.php">Bench</a>
+        </div>
+    </div>
+    <hr/>
 </html>	';
 
-	phpinfo();
+phpinfo();
 ?>
